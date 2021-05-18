813e25e31855d922709afa14472f7445dafa5419
Salasc2 <salasch2@gmail.com>
2021-05-17 17:45:58 -0700

    initial commit

---
+++ src/main/java/CST438Assign3/Cst438Assign3Application.java
@@ -0,0 +1,13 @@
+package CST438Assign3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438Assign3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438Assign3Application.class, args);
+	}
+
+}
